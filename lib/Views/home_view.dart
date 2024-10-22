47aff0d8765a8d3ec41392c81acf291ad2a5904e
Ziad Alaa <ziadalaa955@gmail.com>
2024-10-22 14:19:06 +0300

    create category card widget

--- lib/Views/home_view.dart
+++ lib/Views/home_view.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/Widgets/category_card.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
   static String id = 'homeView';
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'News',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Cloud',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.orange,
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: const CategoryCard(),
+    );
   }
 }
