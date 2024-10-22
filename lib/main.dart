a48774731a325d9d68b2dfd317a2dcd4a1d8db2a
Ziad Alaa <ziadalaa955@gmail.com>
2024-10-22 14:03:42 +0300

    create home view

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/Views/home_view.dart';
+
+void main() {
+  runApp(const NewsApp());
+}
+
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        HomeView.id: (context) => const HomeView(),
+      },
+      initialRoute: HomeView.id,
+    );
+  }
+}
